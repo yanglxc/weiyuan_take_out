345887840e2126d47a731ad0e22976cb1cc92874
Yang <xcyang666@gmail.com>
2023-12-27 16:34:07 +0800

    Added employee code development

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,18 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 处理SQL异常
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException exception){
+        //Duplicate entry 'TEST' for key 'employee.idx_username'
+        String message = exception.getMessage();
+        if (message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            message = split[2] + MessageConstant.ALREADY_EXIST;
+            return Result.error(message);
+        }
+        return Result.error(MessageConstant.UNKNOWN_ERROR);
+    }
 }
